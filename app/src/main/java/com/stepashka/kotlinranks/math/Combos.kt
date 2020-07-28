48098239f24b70a39d7c53990964a31c7aecb107
Albert Yakubov <mrayakubov@hotmail.com>
2020-07-28 10:11:02 -0600

    Update Combos.kt

--- app/src/main/java/com/stepashka/kotlinranks/math/Combos.kt
+++ app/src/main/java/com/stepashka/kotlinranks/math/Combos.kt
@@ -20,3 +20,34 @@ fun ConsecutiveCombos(myArray: IntArray, target: Int) : Int{
     }
         return counter
 }
+
+//optimized version:
+
+fun ConsecutiveCombosOptimized(myArray: IntArray, target: Int) : Int {
+    var sum = myArray[0]
+    var startIndex = 0
+    var endIndex = 0
+    var counter = 0
+    if(myArray == null) {
+        return 0
+    }
+    while (startIndex < myArray.size && endIndex < myArray.size) {
+        if (sum < target) {
+            endIndex++
+
+            // this just makes sure that we are not messing up the count of an array (kind of an edge case so we dont end up on backwards index)
+            if (endIndex >= startIndex){
+                counter = counter + (endIndex - startIndex)
+            }
+            if (endIndex < myArray.size){
+                sum = sum + myArray[endIndex]
+            }
+
+        } else {
+
+            sum = sum - myArray[startIndex]
+            startIndex++
+        }
+    }
+    return counter
+}
