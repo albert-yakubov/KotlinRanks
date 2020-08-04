40d974f0b674616608834b8dbfbe22298bfab03d
Albert Yakubov <mrayakubov@hotmail.com>
2020-08-04 10:14:56 -0600

    kotlinRanks

---
+++ app/src/main/java/com/stepashka/kotlinranks/LRUcache/LRUcache.kt
@@ -0,0 +1,8 @@
+package com.stepashka.kotlinranks.LRUcache
+
+/**
+ * store the key value pairs
+ * keep track of the order of the elements
+ * if the capacity exceeds then 
+ *
+ * */
---
+++ app/src/main/java/com/stepashka/kotlinranks/arrays/Contains.kt
@@ -0,0 +1,12 @@
+package com.stepashka.kotlinranks.arrays
+
+fun longestCommonPrefix(strs: Array<String>): String {
+    /**
+     *
+     *
+     *
+     *
+     *
+     * */
+    return ""
+}
