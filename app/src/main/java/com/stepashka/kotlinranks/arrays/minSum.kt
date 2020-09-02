ec95a1c839c2932fe16c1bfddfda0f719116b405
Albert Yakubov <mrayakubov@hotmail.com>
2020-09-01 23:31:05 -0600

    Update minSum.kt

--- app/src/main/java/com/stepashka/kotlinranks/arrays/minSum.kt
+++ app/src/main/java/com/stepashka/kotlinranks/arrays/minSum.kt
@@ -5,24 +5,36 @@ package com.stepashka.kotlinranks.arrays
  * pull all elements
  * and pick the smallest 4
  * and return the sum of those
+ * (this solution didnt work because I ran into Long which turns into edge case)
  */
 
-fun miniMaxSum(arr: Array<Long>): MutableList<Long> {
-    var nums = 0L
-    var answers = mutableListOf<Long>()
-    arr.sort()
-    nums = (arr[0] + arr[1] + arr[2] + arr[3])
-    var highNums = arr[1] + arr[2] + arr[3] + arr[4]
-    answers.add(nums)
-    var fullSum = arr.sum()
-    answers.add(highNums)
-    return answers
+/***
+ * If you take the sum of all numbers and subtract the highest num
+ * you will get the minimal sum, vise versa for max sum
+ */
 
+fun miniMaxSum(arr: Array<Int>): Unit {
+
+
+    var sumOfAllNumbers = arr.sum()
+    //println(sumOfAllNumbers) // 15
+    var maxOfAllValues = arr.max()
+    //println(maxOfAllValues)  //
+    var minValueOfAllNums = arr.min()
+    var minSum = sumOfAllNumbers- maxOfAllValues!!
+    var maxSum = sumOfAllNumbers - minValueOfAllNums!!
+    println("$minSum $maxSum")
+    return
 
 }
+// 1 + 2 + 3 + 4 = 10
 
-val myArry: Array<Long> = arrayOf(256741038 ,623958417 ,467905213 ,714532089 ,938071625)
+val example: Array<Int> = arrayOf(1, 2, 3, 4, 5)
 
+val myArry: Array<Int> = arrayOf(256741038 ,623958417 ,467905213 ,714532089 ,938071625)
+val myArry2: Array<Int> = arrayOf(1, 3, 5, 7, 9)
 fun main(args: Array<String>){
+    println(miniMaxSum(example))
     println(miniMaxSum(myArry))
+
 }
