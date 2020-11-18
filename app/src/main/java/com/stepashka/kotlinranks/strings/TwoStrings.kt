0d86a9eeff917324a9c2fc77bbb4283926988f20
Albert Yakubov <mrayakubov@hotmail.com>
2020-11-18 13:34:41 -0700

    Update TwoStrings.kt

--- app/src/main/java/com/stepashka/kotlinranks/strings/TwoStrings.kt
+++ app/src/main/java/com/stepashka/kotlinranks/strings/TwoStrings.kt
@@ -1,5 +1,12 @@
 package com.stepashka.kotlinranks.strings
 
 fun contains(s1: String, s2: String): String{
-    
+    val set2 = s2.toSet()
+    s1.toSet().forEach { if(set2.contains(it)) return "YES" }
+    return "NO"
+
+}
+fun main(args: Array<String>){
+    println(contains("hello", "world"))
+    println(contains("sam", "help"))
 }
